01709fcc4f996c8c6ecdb6c45a12e249a43bc851
daiki510 <ddgg7755@gmail.com>
2022-03-20 14:33:44 +0900

    change app2 port

--- apps/app-sample-2/src/main.ts
+++ apps/app-sample-2/src/main.ts
@@ -3,6 +3,6 @@ import { AppSample2Module } from './app-sample-2.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppSample2Module);
-  await app.listen(3000);
+  await app.listen(3001);
 }
 bootstrap();
